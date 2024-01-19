87aa1e01d5d0352dbe7684eb38487ea6fcee7a86
alice <castaldialicee@gmail.com>
2024-01-19 15:54:01 +0100

    Aggiungere i file di progetto.

---
+++ Anni/Persona.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anni
+{
+    internal class Persona
+    {
+        public Persona()
+        {
+        }
+
+        public Persona(int annodinascita)
+        {
+            this.Annodinascita = annodinascita;
+        }
+
+        public int Annodinascita { get; set; }
+        public int Calcolaeta()
+        {
+            return DateTime.Now.Year - Annodinascita;
+        }
+
+    }
+}
---
+++ Anni/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona p=new Persona();
+            Console.WriteLine("inserire l'età:");
+            p.Annodinascita=int.Parse(Console.ReadLine());
+            Console.WriteLine("ha {0} anni", p.Calcolaeta());
+            Console.ReadLine();
+        }
+    }
+}
